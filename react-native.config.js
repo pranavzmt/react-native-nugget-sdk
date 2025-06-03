f1bd6c4606b4ed9f752a8fe6d1739f9bc00f0807
Pranav Singh <singh.pranav@zomato.com>
2025-06-03 19:02:38 +0530

    enabled auto linking

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: {},
+      android: {}, // projects are grouped into "platforms"
+    },
+    assets: [], // stays the same
+    hooks: {}, // stays the same
+  },
+}; 
